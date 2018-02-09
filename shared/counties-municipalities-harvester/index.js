19dbefb7d5ba0e4d08b944e2eb6074086ff71673
Roar Skaar <roar.skaar@gmail.com>
2018-02-09 05:02:23 +0100

    fix(shared/CM): update to use postgresql

--- shared/counties-municipalities-harvester/index.js
+++ shared/counties-municipalities-harvester/index.js
@@ -1,14 +1,15 @@
-// @flow
-
 import fetch from 'isomorphic-fetch';
-import { performance } from 'perf_hooks'; // eslint-disable-line
+import moment from 'moment';
+import uuid4 from 'uuid/v4';
 
 import {
   printDone,
   logError,
   createLogger,
+  startDuration,
+  endDuration,
 } from '@turistforeningen/ntb-shared-utils';
-import models from '@turistforeningen/ntb-shared-models';
+import db from '@turistforeningen/ntb-shared-models';
 
 
 const logger = createLogger();
@@ -33,29 +34,13 @@ const MUNICIPALITIES_API_URL = (
 
 
 // ######################################
-// Flow types
-// ######################################
-
-
-type validStatuses = 'public' | 'deleted' | 'draft';
-
-
-type APIItem = {
-  label: string,
-  uuid: string,
-  description: string,
-  status: 'Gyldig' | 'Utgått' | 'Innsendt',
-}
-
-
-// ######################################
 // Predicate functions
 // ######################################
 
 /**
  * Makes sure the value is a string
  */
-function isString(value: string): %checks {
+function isString(value) {
   return typeof value === 'string' || value instanceof String;
 }
 
@@ -63,7 +48,7 @@ function isString(value: string): %checks {
 /**
  * Makes sure the API item status is valid
  */
-function isValidStatus(status: string): %checks {
+function isValidStatus(status) {
   return (
     status === 'Gyldig' ||
     status === 'Utgått' ||
@@ -80,7 +65,7 @@ function isValidStatus(status: string): %checks {
 /**
  * Translate Kartverket-API status to the correct status name for NTB
  */
-function translateStatus(status: string): validStatuses {
+function translateStatus(status) {
   switch (status.toLowerCase()) {
     case 'utgått':
       return 'deleted';
@@ -93,14 +78,118 @@ function translateStatus(status: string): validStatuses {
 
 
 // ######################################
+// Temp db tables
+// ######################################
+
+
+/**
+ * Create temporary table for holding info on new/updated counties or
+ * municipalities
+ */
+async function createTempTableCM(tableName, municipality = false) {
+  const fields = {
+    uuid: {
+      type: db.Sequelize.UUID,
+      primaryKey: true,
+    },
+    county_uuid: db.Sequelize.UUID,
+    code: db.Sequelize.TEXT,
+    name: db.Sequelize.TEXT,
+    name_lower_case: db.Sequelize.TEXT,
+    status: db.Sequelize.TEXT,
+    data_source: db.Sequelize.TEXT,
+  };
+
+  if (municipality) {
+    fields.county_uuid = db.Sequelize.UUID;
+  }
+
+  const model = db.sequelize.define(tableName, fields, {
+    timestamps: false,
+    tableName,
+  });
+
+  await model.sync();
+  return model;
+}
+
+/**
+ * Create temporary table for holding info on new/updated translations for
+ * counties or municipalities
+ */
+async function createTempTableCMTranslation(tableName) {
+  const model = db.sequelize.define(tableName, {
+    uuid: {
+      type: db.Sequelize.UUID,
+      primaryKey: true,
+    },
+    m_uuid: db.Sequelize.UUID,
+    name: db.Sequelize.TEXT,
+    name_lower_case: db.Sequelize.TEXT,
+    language: db.Sequelize.TEXT,
+    data_source: db.Sequelize.TEXT,
+  }, {
+    timestamps: false,
+    tableName,
+  });
+
+  await model.sync();
+  return model;
+}
+
+/**
+ * Create temporary tables
+ */
+async function createTempTables() {
+  const date = moment().format('YYYYMMDDHHmmssSSS');
+  const baseTableName = `_temp_cm_harvest_${date}`;
+  const c = `${baseTableName}_c`;
+  const ct = `${baseTableName}_ct`;
+  const m = `${baseTableName}_m`;
+  const mt = `${baseTableName}_mt`;
+
+  logger.info('Creating temporary database tables');
+  const durationId = startDuration();
+
+  const County = await createTempTableCM(c);
+  const CountyTranslation = await createTempTableCMTranslation(ct);
+  const Municipality = await createTempTableCM(m, true);
+  const MunicipalityTranslation = await createTempTableCMTranslation(mt);
+
+  endDuration(durationId);
+
+  return {
+    County,
+    CountyTranslation,
+    Municipality,
+    MunicipalityTranslation,
+  };
+}
+
+/**
+ * Drop temporary tables
+ */
+async function dropTempTables(temp) {
+  logger.info('Dropping temporary database tables');
+  const durationId = startDuration();
+
+  await temp.County.drop();
+  await temp.CountyTranslation.drop();
+  await temp.Municipality.drop();
+  await temp.MunicipalityTranslation.drop();
+
+  endDuration(durationId);
+}
+
+// ######################################
 // Harvester
 // ######################################
 
 /**
  * Retrieves data from Kartverket API and prosesses each item to make sure
- * it
+ * it is valid and not old and deprecated
  */
-async function getData(type: string): Promise<APIItem[] | null> {
+async function getData(type) {
   const url = type === 'counties'
     ? COUNTIES_API_URL
     : MUNICIPALITIES_API_URL;
@@ -116,7 +205,7 @@ async function getData(type: string): Promise<APIItem[] | null> {
 
   let data;
   try {
-    performance.mark('a');
+    const durationId = startDuration();
     const res = await fetch(url, options);
 
     if (res.status !== 200) {
@@ -124,8 +213,7 @@ async function getData(type: string): Promise<APIItem[] | null> {
     }
 
     data = await res.json();
-    performance.mark('b');
-    printDone();
+    endDuration(durationId);
   }
   catch (err) {
     logError(err, 'Unable to fetch data from Kartverket API');
@@ -133,7 +221,7 @@ async function getData(type: string): Promise<APIItem[] | null> {
   }
 
   logger.info('Validating data from API');
-  const items: APIItem[] = [];
+  const items = [];
   let allItemsValidated = true;
   let ignoredItems = 0;
 
@@ -198,68 +286,302 @@ async function getData(type: string): Promise<APIItem[] | null> {
 
 
 /**
- * Process the API data into County instances
+ * Process the API data for counties. Add the data to temporary tables,
+ * and use it to create or update existing data
  */
-function processCountyData(data: APIItem[]): models.County[] {
-  const counties = [];
+async function processCountyData(data, temp) {
+  let sql;
+  let durationId;
+  const countyItems = [];
+  const countyTranslationItems = [];
+
+  logger.info('Processing county data');
 
   data.forEach((item) => {
     const names = item.description.split(' - ');
     const name = names[0];
     const smeName = names.length > 1 ? names[1] : null;
-
-    const county = new models.County({
+    countyItems.push({
       uuid: item.uuid,
       code: item.label,
       name,
+      name_lower_case: name.toLowerCase(),
       status: translateStatus(item.status),
       data_source: DATASOURCE_NAME,
     });
-    counties.push(county);
 
     if (smeName) {
-      county.setTranslation('sme', smeName);
+      countyTranslationItems.push({
+        uuid: uuid4(),
+        name: smeName,
+        name_lower_case: smeName.toLowerCase(),
+        language: 'sme',
+        data_source: DATASOURCE_NAME,
+        m_uuid: item.uuid,
+      });
     }
   });
 
-  return counties;
+  // Bulk insert into temp tables
+
+  if (countyItems.length) {
+    logger.info('Inserting counties to temporary table');
+    durationId = startDuration();
+    await temp.County.bulkCreate(countyItems);
+    endDuration(durationId);
+  }
+
+  if (countyTranslationItems.length) {
+    logger.info('Inserting county translations to temporary table');
+    durationId = startDuration();
+    await temp.CountyTranslation.bulkCreate(countyTranslationItems);
+    endDuration(durationId);
+  }
+
+  // Merge counties
+  sql = [
+    'INSERT INTO county (',
+    '  uuid, code, name, name_lower_case, status, data_source, created_at,',
+    '  updated_at',
+    ')',
+    'SELECT',
+    '  uuid, code, name, name_lower_case,',
+    '  status::enum_county_status, data_source, now(), now()',
+    `FROM public.${temp.County.tableName}`,
+    'ON CONFLICT (uuid) DO UPDATE',
+    'SET',
+    '  code = EXCLUDED.code,',
+    '  name = EXCLUDED.name,',
+    '  name_lower_case = EXCLUDED.name_lower_case,',
+    '  status = EXCLUDED.status,',
+    '  updated_at = now()',
+  ].join('\n');
+
+  logger.info('Creating or updating counties');
+  durationId = startDuration();
+  await db.sequelize.query(sql, { raw: true });
+  endDuration(durationId);
+
+  // Merge county translations
+  sql = [
+    'INSERT INTO county_translation (',
+    '  uuid, county_uuid, name, name_lower_case, language, data_source,',
+    '  created_at, updated_at',
+    ')',
+    'SELECT',
+    '  uuid, m_uuid, name, name_lower_case, language, data_source, now(),',
+    '  now()',
+    `FROM public.${temp.CountyTranslation.tableName}`,
+    'ON CONFLICT (county_uuid, language)',
+    'DO UPDATE',
+    'SET',
+    '  name = EXCLUDED.name,',
+    '  name_lower_case = EXCLUDED.name_lower_case,',
+    '  updated_at = now()',
+  ].join('\n');
+
+  logger.info('Creating or updating county translations');
+  durationId = startDuration();
+  await db.sequelize.query(sql, { raw: true });
+  endDuration(durationId);
+
+  // Delete old county translations
+  sql = [
+    'DELETE FROM public.county_translation',
+    'USING public.county_translation ct',
+    `LEFT JOIN public.${temp.CountyTranslation.tableName} te ON`,
+    '  ct.county_uuid = te.m_uuid AND',
+    '  ct.language = te.language',
+    'WHERE',
+    '  te.language IS NULL AND',
+    '  ct.data_source = :data_source AND',
+    '  public.county_translation.uuid = ct.uuid',
+  ].join('\n');
+
+  logger.info('Deleting old county translations');
+  durationId = startDuration();
+  await db.sequelize.query(sql, {
+    raw: true,
+    replacements: { data_source: DATASOURCE_NAME },
+  });
+  endDuration(durationId);
+
+  // Mark counties not found in the new data as deleted
+  sql = [
+    'UPDATE public.county',
+    'SET status = :status, updated_at = now()',
+    'FROM public.county c',
+    `LEFT JOIN public.${temp.County.tableName} te ON`,
+    '  c.uuid = te.uuid',
+    'WHERE',
+    '  te.uuid IS NULL AND',
+    '  c.data_source = :data_source AND',
+    '  public.county.uuid = c.uuid',
+  ].join('\n');
+
+  logger.info('Marking counties not found in the new data as deleted');
+  durationId = startDuration();
+  await db.sequelize.query(sql, {
+    raw: true,
+    replacements: {
+      data_source: DATASOURCE_NAME,
+      status: 'deleted',
+    },
+  });
+  endDuration(durationId);
+
+  return countyItems;
 }
 
 /**
- * Process the API data into Municipality instances
+ * Process the API data for municipalities. Add the data to temporary tables,
+ * and use it to create or update existing data
  */
-function processMunicipalityData(
-  data: APIItem[],
-  counties: models.County[]
-): models.Municipality[] {
-  const municipalities = [];
+async function processMunicipalityData(data, counties, temp) {
+  let sql;
+  let durationId;
+  const items = [];
+  const translationItems = [];
 
   data.forEach((item) => {
     const names = item.description.split(' - ');
     const name = names[0];
     const smeName = names.length > 1 ? names[1] : null;
 
-    const municipality = new models.Municipality({
+    const countyLabel = item.label.substr(0, 2);
+    const county = counties.filter((c) => c.code === countyLabel);
+    const countyUuid = county[0].uuid;
+
+    items.push({
       uuid: item.uuid,
+      county_uuid: countyUuid,
       code: item.label,
       name,
+      name_lower_case: name.toLowerCase(),
       status: translateStatus(item.status),
       data_source: DATASOURCE_NAME,
     });
-    municipalities.push(municipality);
 
     if (smeName) {
-      municipality.setTranslation('sme', smeName);
-    }
-
-    const countyLabel = item.label.substr(0, 2);
-    const county = counties.filter((c) => c.data.code === countyLabel);
-    if (county.length === 1) {
-      municipality.setLocatedIn(county[0]);
+      translationItems.push({
+        uuid: uuid4(),
+        name: smeName,
+        name_lower_case: smeName.toLowerCase(),
+        language: 'sme',
+        data_source: DATASOURCE_NAME,
+        m_uuid: item.uuid,
+      });
     }
   });
 
-  return municipalities;
+  // Bulk insert into temp tables
+
+  if (items.length) {
+    logger.info('Inserting municipalities to temporary table');
+    durationId = startDuration();
+    await temp.Municipality.bulkCreate(items);
+    endDuration(durationId);
+  }
+
+  if (translationItems.length) {
+    logger.info('Inserting municipality translations to temporary table');
+    durationId = startDuration();
+    await temp.MunicipalityTranslation.bulkCreate(translationItems);
+    endDuration(durationId);
+  }
+
+  // Merge counties
+  sql = [
+    'INSERT INTO municipality (',
+    '  uuid, county_uuid, code, name, name_lower_case, status, data_source,',
+    '  created_at, updated_at',
+    ')',
+    'SELECT',
+    '  uuid, county_uuid, code, name, name_lower_case,',
+    '  status::enum_municipality_status, data_source, now(), now()',
+    `FROM public.${temp.Municipality.tableName}`,
+    'ON CONFLICT (uuid) DO UPDATE',
+    'SET',
+    '  county_uuid = EXCLUDED.county_uuid,',
+    '  code = EXCLUDED.code,',
+    '  name = EXCLUDED.name,',
+    '  name_lower_case = EXCLUDED.name_lower_case,',
+    '  status = EXCLUDED.status,',
+    '  updated_at = now()',
+  ].join('\n');
+
+  logger.info('Creating or updating municipalities');
+  durationId = startDuration();
+  await db.sequelize.query(sql, { raw: true });
+  endDuration(durationId);
+
+  // Merge county translations
+  sql = [
+    'INSERT INTO municipality_translation (',
+    '  uuid, municipality_uuid, name, name_lower_case, language, data_source,',
+    '  created_at, updated_at',
+    ')',
+    'SELECT',
+    '  uuid, m_uuid, name, name_lower_case, language, data_source, now(),',
+    '  now()',
+    `FROM public.${temp.MunicipalityTranslation.tableName}`,
+    'ON CONFLICT (municipality_uuid, language)',
+    'DO UPDATE',
+    'SET',
+    '  name = EXCLUDED.name,',
+    '  name_lower_case = EXCLUDED.name_lower_case,',
+    '  updated_at = now()',
+  ].join('\n');
+
+  logger.info('Creating or updating municipality translations');
+  durationId = startDuration();
+  await db.sequelize.query(sql, { raw: true });
+  endDuration(durationId);
+
+  // Delete old municipality translations
+  sql = [
+    'DELETE FROM public.municipality_translation',
+    'USING public.municipality_translation mt',
+    `LEFT JOIN public.${temp.MunicipalityTranslation.tableName} te ON`,
+    '  mt.municipality_uuid = te.m_uuid AND',
+    '  mt.language = te.language',
+    'WHERE',
+    '  te.language IS NULL AND',
+    '  mt.data_source = :data_source AND',
+    '  public.municipality_translation.uuid = mt.uuid',
+  ].join('\n');
+
+  logger.info('Deleting old municipality translations');
+  durationId = startDuration();
+  await db.sequelize.query(sql, {
+    raw: true,
+    replacements: { data_source: DATASOURCE_NAME },
+  });
+  endDuration(durationId);
+
+  // Mark counties not found in the new data as deleted
+  sql = [
+    'UPDATE public.municipality',
+    'SET status = :status, updated_at = now()',
+    'FROM public.municipality m',
+    `LEFT JOIN public.${temp.Municipality.tableName} te ON`,
+    '  m.uuid = te.uuid',
+    'WHERE',
+    '  te.uuid IS NULL AND',
+    '  m.data_source = :data_source AND',
+    '  public.municipality.uuid = m.uuid',
+  ].join('\n');
+
+  logger.info('Marking municipalities not found in the new data as deleted');
+  durationId = startDuration();
+  await db.sequelize.query(sql, {
+    raw: true,
+    replacements: {
+      data_source: DATASOURCE_NAME,
+      status: 'deleted',
+    },
+  });
+  endDuration(durationId);
 }
 
 
@@ -267,24 +589,37 @@ function processMunicipalityData(
  * Harvest data from Kartverket-API and populate the Neo4j-database.
  * Return `true` on success, and `false` on failure
  */
-async function harvest(session: neo4j$session): Promise<boolean> {
+async function harvest() {
+  logger.info('Harvesting counties and municipalities from Kartverket');
+  const durationId = startDuration();
+
   const countiesData = await getData('counties');
   if (countiesData === null) {
+    logger.error('No counties found');
+    endDuration(durationId);
     return false;
   }
 
   const municipalityData = await getData('municipalities');
   if (municipalityData === null) {
+    logger.error('No municipalities found');
+    endDuration(durationId);
     return false;
   }
 
-  const counties = processCountyData(countiesData);
-  await models.County.saveAll(session, counties, DATASOURCE_NAME);
+  // Create temp tables
+  const temp = await createTempTables();
 
-  const municipalities = processMunicipalityData(municipalityData, counties);
-  await models.Municipality.saveAll(session, municipalities, DATASOURCE_NAME);
+  // Process and insert data to database
+  const counties = await processCountyData(countiesData, temp);
+  await processMunicipalityData(municipalityData, counties, temp);
+
+  // Drop temp tables
+  await dropTempTables(temp);
 
   // Success
+  logger.info('Harvesting done');
+  endDuration(durationId);
   return true;
 }
 
