a0a9986b6506695cb0585933575eea39df84d982
Roar Skaar <roar.skaar@gmail.com>
2018-02-10 08:28:33 +0100

    chore(postgres): create postgres with postgis dockerfile

---
+++ postgres/locale.md
@@ -0,0 +1,4 @@
+en_GB
+en_US
+nb_NO
+nn_NO
--- run-postgres.sh
+++ run-postgres.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --publish=5432:5432 --volume=$PWD/postgres/data:/var/lib/postgresql/data --name ntb-postgres -e POSTGRES_PASSWORD=ntb -e POSTGRES_USER=ntb -e POSTGRES_DB=ntb postgres
+docker run --publish=5432:5432 --name ntb-postgres -e POSTGRES_PASSWORD=ntb -e POSTGRES_USER=ntb -e POSTGRES_DB=ntb ntb-postgres
