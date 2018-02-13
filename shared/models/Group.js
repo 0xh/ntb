09ca4826899ab28a5089c963b6871a2778f667f4
Roar Skaar <roar.skaar@gmail.com>
2018-02-13 07:33:46 +0100

    feat(shared/models): add group model

---
+++ shared/models/Group.js
@@ -0,0 +1,103 @@
+export default (sequelize, DataTypes) => {
+  const Group = sequelize.define('Group', {
+    uuid: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      validate: {
+        isUUID: 4,
+      },
+    },
+
+    idLegacyNtb: {
+      type: DataTypes.TEXT,
+      unique: true,
+      allowNull: true,
+    },
+
+    type: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    name: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true,
+      },
+    },
+
+    nameLowerCase: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true,
+      },
+    },
+
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    descriptionPlain: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    descriptionWords: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      allowNull: true,
+    },
+
+    descriptionWordsStemmed: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+      allowNull: true,
+    },
+
+    logo: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    organizationNumber: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    license: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    provider: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    status: {
+      type: DataTypes.ENUM,
+      values: ['draft', 'public', 'deleted', 'private'],
+    },
+
+    dataSource: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  }, {
+    timestamps: true,
+  });
+
+
+  // Associations
+
+  Group.associate = (models) => {
+    models.Group.belongsTo(models.Municipality);
+  };
+
+  return Group;
+};
