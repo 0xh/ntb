03b915ff0c17e9a5ac27fe52de4e10d70f352c46
Roar Skaar <roar.skaar@gmail.com>
2018-02-19 05:21:20 +0100

    fix(shared/nodehun): load dictionaries on application startup

--- shared/hunspell/index.js
+++ shared/hunspell/index.js
@@ -1,7 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-import Nodehun from 'nodehun';
+// Nodehun is in a special file format and eslint does not recognize it
+import Nodehun from 'nodehun'; // eslint-disable-line
+
+
+const DICTIONARY_NAMES = ['nb', 'nn', 'en_us', 'en_gb'];
 
 
 const dictionaries = {};
@@ -9,27 +13,44 @@ const stopWords = {};
 const cleanRegex = /^[^a-zA-Z\u00C0-\u017F0-9]+|[^a-zA-Z\u00C0-\u017F0-9]+$/gi;
 
 
+// Load each dictionary and stop words list to memory
+DICTIONARY_NAMES.forEach((name) => {
+  const affbuf = fs.readFileSync(path.resolve(
+    __dirname, 'dictionaries', `${name}.aff`,
+  ));
+  const dictbuf = fs.readFileSync(path.resolve(
+    __dirname, 'dictionaries', `${name}.dic`,
+  ));
+
+  dictionaries[name] = new Nodehun(affbuf, dictbuf);
+
+  const buf = fs.readFileSync(path.resolve(
+    __dirname, 'stop-words', `${name}.txt`
+  ), 'utf-8');
+  const sw = buf.split('\n').map((w) => w.trim());
+  stopWords[name] = sw;
+});
+
+
+/**
+ * Return Nodehun instance of the named dictionary
+ */
 export default function getDictionary(dictionaryName) {
-  if (!dictionaries[dictionaryName]) {
-    const affbuf = fs.readFileSync(path.resolve(
-      __dirname, 'dictionaries', `${dictionaryName}.aff`,
-    ));
-    const dictbuf = fs.readFileSync(path.resolve(
-      __dirname, 'dictionaries', `${dictionaryName}.dic`,
-    ));
-
-    dictionaries[dictionaryName] = new Nodehun(affbuf, dictbuf);
-  }
-
   return dictionaries[dictionaryName];
 }
 
 
+/**
+ * Clean word of leading and trailing dots, dashes and so on
+ */
 export function cleanWord(word) {
   return word.replace(cleanRegex, '').trim();
 }
 
 
+/**
+ * Clean words of leading and trailing dots, dashes and so on
+ */
 export function cleanWords(words) {
   return words
     .map((w) => w.replace(cleanRegex, '').trim())
@@ -37,19 +58,17 @@ export function cleanWords(words) {
 }
 
 
+/**
+ * Remove stop words by using the named dictionary
+ */
 export function clearStopWords(dictionaryName, words) {
-  if (!stopWords[dictionaryName]) {
-    const buf = fs.readFileSync(path.resolve(
-      __dirname, 'stop-words', `${dictionaryName}.txt`
-    ), 'utf-8');
-    const sw = buf.split('\n').map((w) => w.trim());
-    stopWords[dictionaryName] = sw;
-  }
-
   return words.filter((w) => !stopWords[dictionaryName].includes(w));
 }
 
 
+/**
+ * Stem each word using Hunspell
+ */
 export async function stemAll(dictionaryName, words, _clearStopWords = true) {
   const dict = getDictionary(dictionaryName);
   let stemmed = [];
