c0d705d94677242501bc7e43a9a61e3085ecd299
Roar Skaar <roar.skaar@gmail.com>
2018-04-06 13:13:25 +0200

    fix(docker): docker compose configuration

--- tools/docker-compose-assets/postgres/ntb-db-setup.sh
+++ tools/docker-compose-assets/postgres/ntb-db-setup.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
-
-echo "This script is using PGDATABASE ntb and OWNER ntb  ..."
-
-echo "Checking if ntb exists ..."
-
-# psql uses ENV vars for PGHOST, PGDATABASE, PGUSER and PGPASSWORD to connect to the target db.
-if psql -lqt | cut -d \| -f 1 | grep -qw ntb; then
-    # database exists. It is assumed that it exists with the right collation.
-    echo "ntb exists ... normal exit."
-    exit 0
+#  checking if ENV variables are set
+if [ -z "$PGDATABASE" ] || [ -z "$PGUSER" ] || [ -z "$PGPASSWORD" ] || [ -z "$PGHOST" ]; then
+  echo "ENV variables are not defined. Aborting!"
+  exit 1;
 else
-    # create the DB with the right collation and postgis extension
-    echo "creating DB ... "
-    if psql postgres -c "create database \"ntb\" owner ntb encoding=\"UTF8\" LC_COLLATE=\"nb_NO.UTF8\" LC_CTYPE=\"nb_NO.UTF8\" template=template0;"; then
-      echo "DB created successfully!"
-      echo "installing postgis extension ..."
-      if psql -d ntb -c "CREATE EXTENSION postgis;"; then
-        echo "postgis extension installed successfully!"
-        echo "installing other extensions ..."
-        psql -d ntb -c "CREATE EXTENSION postgis_topology;"
-        psql -d ntb -c "CREATE EXTENSION fuzzystrmatch;"
-        psql -d ntb -c "CREATE EXTENSION postgis_tiger_geocoder;"
-        psql -d ntb -c "CREATE EXTENSION \"uuid-ossp\";"
+  echo "This script is using PGDATABASE $PGDATABASE and OWNER $PGUSER  ..."
+
+  echo "Checking if $PGDATABASE  exists ..."
+
+  # psql uses ENV vars for PGHOST, PGDATABASE, PGUSER and PGPASSWORD to connect to the target db.
+  if psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE; then
+      # database exists. It is assumed that it exists with the right collation.
+      echo "$PGDATABASE exists ... normal exit."
+      exit 0
+  else
+      # create the DB with the right collation and postgis extension
+      echo "creating DB ... "
+      if psql postgres -c "create database \"$PGDATABASE\" owner $PGUSER encoding=\"UTF8\" LC_COLLATE=\"nb_NO.UTF8\" LC_CTYPE=\"nb_NO.UTF8\" template=template0;"; then
+        echo "DB created successfully!"
+        echo "installing postgis extension ..."
+        if psql -d $PGDATABASE -c "CREATE EXTENSION postgis;"; then
+          echo "postgis extension installed successfully!"
+          echo "installing other extensions ..."
+          psql -d $PGDATABASE -c "CREATE EXTENSION postgis_topology;"
+          psql -d $PGDATABASE -c "CREATE EXTENSION fuzzystrmatch;"
+          psql -d $PGDATABASE -c "CREATE EXTENSION postgis_tiger_geocoder;"
+          psql -d $PGDATABASE -c "CREATE EXTENSION \"uuid-ossp\";"
+        else
+          echo "ERROR: failed to install postgis extension. Aborting!"
+          exit 1
+        fi
+        echo "DB $PGDATABASE is now successfully created and setup."
       else
-        echo "ERROR: failed to install postgis extension. Aborting!"
+        echo "ERROR: failed to create $PGDATABASE DB. Aborting!"
         exit 1
       fi
-      echo "DB ntb is now successfully created and setup."
-    else
-      echo "ERROR: failed to create ntb DB. Aborting!"
-      exit 1
-    fi
+  fi
 fi
