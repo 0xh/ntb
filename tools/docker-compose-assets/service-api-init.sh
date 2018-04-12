8ea2af053adbbcb3827cb28a25bd9ecbc8cd8569
Roar Skaar <roar.skaar@gmail.com>
2018-04-12 04:54:33 +0200

    chore(docker-compose): add --inspect parameter to nodemon for services/api

--- tools/docker-compose-assets/service-api-init.sh
+++ tools/docker-compose-assets/service-api-init.sh
@@ -3,4 +3,4 @@ echo "Init script: Making sure lerna is bootstrapped"
 lerna bootstrap
 
 echo "Init script: Booting up nodemon"
-/build/node_modules/.bin/nodemon -e js,json --watch /build/shared --watch /build/services/api --watch /build/settings-dev.json --exec "/build/node_modules/.bin/babel-node /build/services/api/index.js"
+/build/node_modules/.bin/nodemon --inspect=0.0.0.0:9229 -e js,json --watch /build/shared --watch /build/services/api --watch /build/settings-dev.json --exec "/build/node_modules/.bin/babel-node /build/services/api/index.js"
