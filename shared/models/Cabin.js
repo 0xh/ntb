c71fbda6521d413d75b829f61a4c524cb0d5559b
Roar Skaar <roar.skaar@gmail.com>
2018-04-28 06:55:36 +0200

    feat(shared/models): add APIConfiguration of Cabin

--- shared/models/Cabin.js
+++ shared/models/Cabin.js
@@ -1,5 +1,5 @@
 export default (sequelize, DataTypes) => {
-  const Cabin = sequelize.define('Cabin', {
+  const attributeConfig = {
     uuid: {
       type: DataTypes.UUID,
       primaryKey: true,
@@ -140,9 +140,14 @@ export default (sequelize, DataTypes) => {
       default: 1,
       allowNull: false,
     },
-  }, {
+  };
+
+  const modelConfig = {
     timestamps: true,
-  });
+  };
+
+
+  const Cabin = sequelize.define('Cabin', attributeConfig, modelConfig);
 
 
   // Associations
@@ -231,6 +236,242 @@ export default (sequelize, DataTypes) => {
   Cabin.hook('beforeSave', (instance) => {
     instance.nameLowerCase = instance.name.toLowerCase();
   });
+
+
+  // API CONFIGURATION
+
+  Cabin.APIEntryModel = true;
+
+  Cabin.getAPIConfig = (models) => {
+    const config = { byReferrer: {} };
+
+    // Configuration when it's the entry model
+    config.byReferrer['*onEntry'] = {
+      paginate: true,
+      fullTextSearch: true,
+      ordering: true,
+
+      defaultLimit: 10,
+      maxLimit: 50,
+      validOrderFields: [
+        'name',
+        'updatedAt',
+        'createdAt',
+      ],
+      defaultOrder: [['name', 'ASC']],
+      // validFields - true/false if they should be returned from API as
+      // default if no ?fields=.. parameter is specified
+      validFields: {
+        uri: true,
+        id: true,
+        idSsr: true,
+        dntCabin: true,
+        dntDiscount: true,
+        name: true,
+        nameAlt: true,
+        description: true,
+        contactDetails: true, // constructed object
+        url: true,
+        yearOfConstruction: true,
+        coordinate: true,
+        map: true,
+        serviceLevel: true,
+        beds: true, // constructed object
+        booking: true, // constructed object
+        htgt: true, // constructed object
+        license: true,
+        provider: true,
+        status: true,
+        dataSource: true,
+        updatedAt: true,
+        createdAt: false,
+      },
+      include: {
+        // maintainerGroup
+        // ownerGroup
+        // contactGroup
+        // county
+        // municipality
+
+        // parents: {
+        //   includeByDefault: true,
+        //   model: models.Area,
+        //   through: {
+        //     association: 'AreaToAreaParent',
+        //     reverseAssociation: 'Parent',
+        //     foreignKey: 'childUuid',
+        //     otherKey: 'parentUuid',
+        //   },
+        // },
+        // children: {
+        //   includeByDefault: true,
+        //   model: models.Area,
+        //   through: {
+        //     association: 'AreaToAreaChild',
+        //     reverseAssociation: 'Child',
+        //     foreignKey: 'parentUuid',
+        //     otherKey: 'childUuid',
+        //   },
+        // },
+      },
+    };
+
+    // Default configuration when included from another model
+    config.byReferrer.default = {
+      ...config.byReferrer['*onEntry'],
+
+      validFields: {
+        // Allow the same fields as '*onEntry' but set them to default false
+        ...Object.assign(
+          {},
+          ...(
+            Object.keys(config.byReferrer['*onEntry'].validFields)
+              .map((f) => ({
+                [f]: false,
+              }))
+          )
+        ),
+        uri: true,
+        id: true,
+        name: true,
+      },
+
+      include: {
+        // parents: {
+        //   ...config.byReferrer['*onEntry'].include.parents,
+        //   includeByDefault: false,
+        // },
+        // children: {
+        //   ...config.byReferrer['*onEntry'].include.children,
+        //   includeByDefault: false,
+        // },
+      },
+    };
+
+    return config;
+  };
+
+  Cabin.fieldsToAttributes = (fields) => {
+    const attributes = [].concat(...fields.map((field) => {
+      switch (field) {
+        case 'uri':
+          return null;
+        case 'id':
+          return ['uuid'];
+        case 'contactDetails':
+          return [
+            'contactName',
+            'email',
+            'phone',
+            'mobile',
+            'fax',
+            'address1',
+            'address2',
+            'postalCode',
+            'postalName',
+          ];
+        case 'beds':
+          return [
+            'bedsExtra',
+            'bedsServiced',
+            'bedsSelfService',
+            'bedsUnmanned',
+            'bedsWinter',
+          ];
+        case 'booking':
+          return [
+            'bookingEnabled',
+            'bookingOnly',
+            'bookingUrl',
+          ];
+        case 'htgt':
+          return [
+            'htgtGeneral',
+            'htgtWinter',
+            'htgtSummer',
+            'htgtPublicTransport',
+            'htgtCarAllYear',
+            'htgtCarSummer',
+            'htgtBicycle',
+            'htgtPublicTransportAvailable',
+            'htgtBoatTransportAvailable',
+          ];
+        case 'createdAt':
+        case 'updatedAt':
+          if (modelConfig.timestamps) {
+            return [field];
+          }
+          throw new Error(`Unable to translate field ${field} on Area model`);
+        default:
+          if (Object.keys(attributeConfig).includes(field)) {
+            return [field];
+          }
+          throw new Error(`Unable to translate field ${field} on Area model`);
+      }
+    }).filter((field) => field !== null));
+
+    return attributes;
+  };
+
+
+  Cabin.prototype.format = function format() {
+    return {
+      uri: `cabin/${this.uuid}`,
+      id: this.uuid,
+      idSsr: this.idSsr,
+      dntCabin: this.dntCabin,
+      dntDiscount: this.dntDiscount,
+      name: this.name,
+      nameAlt: this.nameAlt,
+      description: this.description,
+      contactDetails: {
+        name: this.contactName,
+        email: this.email,
+        phone: this.phone,
+        mobile: this.mobile,
+        fax: this.fax,
+        address1: this.address1,
+        address2: this.address2,
+        postalCode: this.postalCode,
+        postalName: this.postalName,
+      },
+      url: this.url,
+      yearOfConstruction: this.yearOfConstruction,
+      coordinate: this.coordinate,
+      map: this.map,
+      serviceLevel: this.serviceLevel,
+      beds: {
+        staffed: this.bedsServiced,
+        selfService: this.bedsSelfService,
+        extra: this.bedsExtra,
+        noService: this.bedsUnmanned,
+        winter: this.bedsWinter,
+      },
+      booking: {
+        enabled: this.bookingEnabled,
+        onlyBooking: this.bookingOnly,
+        url: this.bookingUrl,
+      },
+      htgt: {
+        general: this.htgtGeneral,
+        winter: this.htgtWinter,
+        summer: this.htgtSummer,
+        publicTransport: {
+          available: this.htgtPublicTransportAvailable,
+          description: this.htgtPublicTransport,
+        },
+        boatTransport: this.htgtBoatTransportAvailable,
+        carAllYear: this.htgtCarAllYear,
+        carSummer: this.htgtCarSummer,
+        bicycle: this.htgtBicycle,
+      },
+      license: this.license,
+      provider: this.provider,
+      status: this.status,
+      updatedAt: this.updatedAt,
+      createdAt: this.createdAt,
+    };
+  };
 
   return Cabin;
 };
