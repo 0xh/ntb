9d1d06ae41dcc24c9702b7ffa436ac4c40f32b5d
Roar Skaar <roar.skaar@gmail.com>
2018-04-30 09:23:31 +0200

    feat(shared/models): add APIConfiguration to CabinFacility

--- shared/models/CabinFacility.js
+++ shared/models/CabinFacility.js
@@ -1,5 +1,5 @@
 export default (sequelize, DataTypes) => {
-  const CabinFacility = sequelize.define('CabinFacility', {
+  const attributeConfig = {
     facilityName: {
       type: DataTypes.TEXT,
       allowNull: false,
@@ -14,9 +14,85 @@ export default (sequelize, DataTypes) => {
     description: { type: DataTypes.TEXT },
 
     dataSource: { type: DataTypes.TEXT },
-  }, {
+  };
+
+  const modelConfig = {
     timestamps: false,
-  });
+  };
+
+  const CabinFacility = sequelize.define(
+    'CabinFacility',
+    attributeConfig,
+    modelConfig
+  );
+
+
+  // API CONFIGURATION
+
+  CabinFacility.getAPIConfig = (models) => {
+    const config = { byReferrer: {} };
+
+    // Configuration when it's the entry model
+    config.byReferrer.default = {
+      paginate: true,
+      fullTextSearch: false,
+      ordering: false,
+      defaultOrder: [['facilityName', 'ASC']],
+      defaultLimit: 1,
+      maxLimit: 50,
+      validOrderFields: ['facilityName'],
+      // validFields - true/false if they should be returned from API as
+      // default if no ?fields=.. parameter is specified
+      validFields: {
+        name: true,
+        cabinUuid: false,
+        description: true,
+        updatedAt: false,
+        createdAt: false,
+      },
+      include: {
+        // TODO(Roar):
+        // cabins
+      },
+    };
+
+    return config;
+  };
+
+  CabinFacility.fieldsToAttributes = (fields) => {
+    const attributes = [].concat(...fields.map((field) => {
+      switch (field) {
+        case 'name':
+          return 'facilityName';
+        case 'createdAt':
+        case 'updatedAt':
+          if (modelConfig.timestamps) {
+            return [field];
+          }
+          throw new Error(`Unable to translate field ${field} on Cabin model`);
+        default:
+          if (Object.keys(attributeConfig).includes(field)) {
+            return [field];
+          }
+          throw new Error(`Unable to translate field ${field} on Cabin model`);
+      }
+    }).filter((field) => field !== null));
+
+    return attributes;
+  };
+
+
+  CabinFacility.prototype.format = function format() {
+    return {
+      name: this.facilityName,
+      cabinUuid: this.cabinUuid,
+      description: this.description && this.description.length
+        ? this.description
+        : null,
+      updatedAt: this.updatedAt,
+      createdAt: this.createdAt,
+    };
+  };
 
   return CabinFacility;
 };
