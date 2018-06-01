b857dcc680b0e825bf000c99936b004480ab30b4
Roar Skaar <roar.skaar@gmail.com>
2018-06-01 11:08:18 +0200

    fix(services/admin): remove debug log statement

--- services/admin/server/utils/load-from-webpack-dev-server.js
+++ services/admin/server/utils/load-from-webpack-dev-server.js
@@ -4,7 +4,6 @@ import http from 'http';
 export default function (path) {
   const promise = new Promise((resolve, reject) => {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-    console.log(`http://admin-client:3000/${path}`);  // eslint-disable-line
     http.get(`http://admin-client:3000/${path}`, (res) => {
       res.setEncoding('utf8');
       let rawData = '';
