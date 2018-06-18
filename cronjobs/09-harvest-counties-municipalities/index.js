1ff69ba6f99603e7e3a022539adef2e1b895de01
Roar Skaar <roar.skaar@gmail.com>
2018-06-18 05:20:31 +0200

    chore(cronjobs/09): test wfs downloading of counties an municipalities [WIP]

--- cronjobs/09-harvest-counties-municipalities/index.js
+++ cronjobs/09-harvest-counties-municipalities/index.js
@@ -1,24 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+
+import moment from 'moment';
+
 import { createLogger } from '@turistforeningen/ntb-shared-utils';
-import harvest from
-  '@turistforeningen/ntb-shared-counties-municipalities-harvester';
+import { knex } from '@turistforeningen/ntb-shared-db-utils';
+import wfsUtils from '@turistforeningen/ntb-shared-wfs-utils';
 
 
 const logger = createLogger();
 
+const SRS_NAME = 'urn:ogc:def:crs:EPSG::25833';
+const FOLDER = path.resolve(__dirname, 'data');
 
-harvest()
+const WFS = (
+  'https://wfs.geonorge.no/skwms1/wfs.elf-au?service=WFS&version=2.0.0' +
+  `&request=GetFeature&typeName=au:AdministrativeUnit&srsName=${SRS_NAME}`
+);
+
+
+async function downloadData() {
+  const wfsTableName = await wfsUtils(WFS);
+
+  if (wfsTableName === false) {
+    logger.warn('Downloading wfs failed.');
+    return false;
+  }
+
+  return true;
+}
+
+
+downloadData()
   .then((status) => {
-    if (status) {
-      logger.info('Done with success!');
-      process.exit(0);
-    }
-    else {
-      logger.error('Done with error!');
-      process.exit(1);
-    }
-  })
-  .catch((err) => {
-    logger.error('UNCAUGHT ERROR');
-    logger.error(err.stack);
-    process.exit(1);
+    console.log('ALL DONE', status);  // eslint-disable-line
+    console.log('****');  // eslint-disable-line
   });
