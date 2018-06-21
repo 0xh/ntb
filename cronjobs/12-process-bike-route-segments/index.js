43f3291ac10d1274129f94d2b56b03d7e407e224
Roar Skaar <roar.skaar@gmail.com>
2018-06-21 06:06:57 +0200

    feat(cronjobs): add and update a lot of cronjobs

---
+++ cronjobs/12-process-bike-route-segments/index.js
@@ -0,0 +1,31 @@
+import processRouteSegments
+  from '@turistforeningen/ntb-shared-process-route-segments';
+import { createLogger } from '@turistforeningen/ntb-shared-utils';
+
+
+const logger = createLogger();
+
+
+const type = 'bike';
+const wfsTable = 'routes_wfs_data_bike';
+const unnestCodes = (
+  'string_to_array(substring(a.rutenummer ' +
+  'from \'\\([0-9]+:(.+)\\)\'), \',\')'
+); // (2:xx,xx)
+const unnestMaintainers = (
+  'string_to_array(substring(a.vedlikeholdsansvarlig ' +
+  'from \'\\([0-9]+:(.+)\\)\'), \',\')'
+);
+
+
+processRouteSegments(type, wfsTable, unnestCodes, unnestMaintainers)
+  .then((res) => {
+    logger.debug('ALL DONE');
+    process.exit(0);
+  })
+  .catch((err) => {
+    logger.error('UNCAUGHT ERROR');
+    logger.error(err);
+    logger.error(err.stack);
+    process.exit(1);
+  });
