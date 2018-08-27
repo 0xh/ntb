d44c78d3db5f60c201f0bf1e6279a42e02fe4719
Roar Skaar <roar.skaar@gmail.com>
2018-08-27 08:50:22 +0200

    fix(cronjobs/08): add option for partial or full harvest

--- cronjobs/08-harvest-legacy-ntb-lists/index.js
+++ cronjobs/08-harvest-legacy-ntb-lists/index.js
@@ -6,12 +6,17 @@ import { harvestLists } from
 const logger = createLogger();
 
 
-let limit = 2000;
+let fullHarvest = true;
 if (process.argv.length > 2 && isNumber(process.argv[2])) {
-  limit = +process.argv[2];
+  fullHarvest = process.argv[2] === 'true';
 }
 
-harvestLists(limit)
+let limit = 2000;
+if (process.argv.length > 3 && isNumber(process.argv[3])) {
+  limit = +process.argv[3];
+}
+
+harvestLists(limit, fullHarvest)
   .then(() => process.exit(0))
   .catch((err) => {
     logger.error('UNCAUGHT ERROR');
