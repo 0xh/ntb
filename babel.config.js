d68bb5b167fde0e14fb8436f1d083630559f0c0d
Roar Skaar <roar.skaar@gmail.com>
2018-09-08 05:00:16 +0200

    chore(babel): add typescript preset to babel

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,7 @@ module.exports = function babelConfig(api) {
     retainLines: true,
     presets: [
       ['@babel/env', { targets: { node: 'current' } }],
+      '@babel/typescript',
     ],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
