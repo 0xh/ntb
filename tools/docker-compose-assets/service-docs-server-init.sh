d709acf132872a3f5bdeaf563431127f04c3db45
Roar Skaar <roar.skaar@gmail.com>
2018-09-10 06:11:36 +0200

    chore(tools): change docker-compose init files

--- tools/docker-compose-assets/service-docs-server-init.sh
+++ tools/docker-compose-assets/service-docs-server-init.sh
@@ -4,4 +4,5 @@ echo "Init script: Making sure lerna is bootstrapped"
 lerna bootstrap
 
 echo "Init script: Booting up nodemon"
-/build/node_modules/.bin/nodemon --inspect=0.0.0.0:9227 -e js,json --watch /build/shared --watch /build/services/docs/server --watch /build/settings-dev.json --exec "/build/node_modules/.bin/babel-node /build/services/docs/server/index.js"
+export TS_NODE_FILES=true
+/build/node_modules/.bin/nodemon --inspect=0.0.0.0:9227 -e js,json,ts --watch /build/shared --watch /build/services/docs/server --watch /build/settings-dev.json --exec "node -r ts-node/register /build/services/docs/server/index.js"
