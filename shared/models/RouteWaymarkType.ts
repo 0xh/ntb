319196c3e206cc67fdb45abd4a2cad5e95f1adbc
Roar Skår <roar.skaar@gmail.com>
2018-09-10 03:39:20 +0200

    fix(shared/models): update RouteWaymarkType with typescript

--- shared/models/RouteWaymarkType.ts
+++ shared/models/RouteWaymarkType.ts
@@ -1,10 +1,19 @@
-import Document from './Document';
+import { RelationMappings, JsonSchema } from '@ntb/db-utils';
+
+import Document, {
+  apiConfigPerReferrer,
+  apiConfig,
+} from './Document';
 
 
 export default class RouteWaymarkType extends Document {
   static tableName = 'routeWaymarkTypes';
   static idColumn = 'name';
   static virtualAttributes = ['uri'];
+
+  // Database columns
+  readonly name!: string;
+  description?: string;
 
 
   get uri() {
@@ -46,16 +55,15 @@ export default class RouteWaymarkType extends Document {
   static apiEntryModel = true;
 
   static getApiConfigPerReferrer(): apiConfigPerReferrer {
-    const config = {};
-
     // Configuration when it's the entry model
-    config['*list'] = {
+    const list: apiConfig = {
       paginate: false,
       fullTextSearch: false,
-      ordering: true,
-      defaultOrder: [['name', 'ASC']],
-      validOrderFields: ['name'],
-      validFilters: {
+      ordering: {
+        default: [['name', 'ASC']],
+        validFields: ['name'],
+      },
+      filters: {
         name: { filterTypes: ['=', '$in', '$nin'] },
       },
       fullFields: [
@@ -70,22 +78,28 @@ export default class RouteWaymarkType extends Document {
     };
 
     // Default configuration when an instance in accessed directly
-    config['*single'] = config['*list'];
+    const single: apiConfig = list;
 
     // Default configuration when included from another model
-    config.default = {
-      ...config['*list'],
+    const standard: apiConfig = {
+      ...list,
       defaultFields: ['name'],
     };
 
-    return config;
+    return {
+      standard,
+      '*list': list,
+      '*single': single,
+    };
   }
 
 
   static getAPIFieldsToAttributes(referrers: string[], fields: string[]) {
     const extra = {};
 
-    const attributes = super.getAPIFieldsToAttributes(referrer, fields, extra);
+    const attributes = super.getAPIFieldsToAttributes(
+      referrers, fields, extra,
+    );
 
     return attributes;
   }
