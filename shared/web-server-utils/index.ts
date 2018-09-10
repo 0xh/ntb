e0b2e425594ae1a2d6f1f8acb4a636504bb31e4b
Roar Skår <roar.skaar@gmail.com>
2018-09-10 18:40:00 +0200

    fix(shared/web-server-utils): add type exports for express

--- shared/web-server-utils/index.ts
+++ shared/web-server-utils/index.ts
@@ -1,11 +1,14 @@
 import _bodyParser from 'body-parser';
-import _express from 'express';
+import _express, {
+  Request as _Request,
+  Response as _Response,
+  NextFunction as _NextFunction
+} from 'express';
 import _morgan from 'morgan';
 import _nunjucks from 'nunjucks';
 import _Raven from 'raven';
 import _responseTime from 'response-time';
 import _Redis from 'ioredis';
-
 
 export const bodyParser = _bodyParser;
 export const express = _express;
@@ -14,3 +17,8 @@ export const nunjucks = _nunjucks;
 export const Raven = _Raven;
 export const responseTime = _responseTime;
 export const Redis = _Redis;
+
+
+export type Request = _Request;
+export type Response = _Response;
+export type NextFunction = _NextFunction;
