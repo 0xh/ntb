c01c3ad4808359fad9d4002320bdb418c59284bc
Roar Skår <roar.skaar@gmail.com>
2018-09-10 05:52:20 +0200

    refactor(shared/web-server-utils): change export method

--- shared/web-server-utils/index.ts
+++ shared/web-server-utils/index.ts
@@ -1,7 +1,16 @@
-export { default as bodyParser } from 'body-parser';
-export { default as express } from 'express';
-export { default as morgan } from 'morgan';
-export { default as nunjucks } from 'nunjucks';
-export { default as Raven } from 'raven';
-export { default as responseTime } from 'response-time';
-export { default as Redis } from 'ioredis';
+import _bodyParser from 'body-parser';
+import _express from 'express';
+import _morgan from 'morgan';
+import _nunjucks from 'nunjucks';
+import _Raven from 'raven';
+import _responseTime from 'response-time';
+import _Redis from 'ioredis';
+
+
+export const bodyParser = _bodyParser;
+export const express = _express;
+export const morgan = _morgan;
+export const nunjucks = _nunjucks;
+export const Raven = _Raven;
+export const responseTime = _responseTime;
+export const Redis = _Redis;
