123433d309c0326a5e68f2454227180dc13d2d60
Roar Skår <roar.skaar@gmail.com>
2018-09-10 05:01:09 +0200

    refactor(curstom-typings): lint jsonSchema definition

--- custom-typings/jsonSchema.d.ts
+++ custom-typings/jsonSchema.d.ts
@@ -15,35 +15,34 @@ type DynamicDefault = BuiltinDynamics | string;
 type DynamicDefaultWithArgs = { func: DynamicDefault, [key: string]: any };
 
 interface KeywordAugmentations {
-// From ajv-keywords
-dynamicDefaults?: {
-  [key: string]: DynamicDefault | DynamicDefaultWithArgs;
-}
+  // From ajv-keywords
+  dynamicDefaults?: {
+    [key: string]: DynamicDefault | DynamicDefaultWithArgs;
+  }
 
-format?:
-  | 'date'
-  | 'time'
-  | 'date-time'
-  | 'uri'
-  | 'uri-reference'
-  | 'uri-template'
-  | 'url'
-  | 'email'
-  | 'hostname'
-  | 'ipv4'
-  | 'ipv6'
-  | 'regex'
-  | 'uuid'
-  | 'json-pointer'
-  | 'relative-json-pointer';
+  format?:
+    | 'date'
+    | 'time'
+    | 'date-time'
+    | 'uri'
+    | 'uri-reference'
+    | 'uri-template'
+    | 'url'
+    | 'email'
+    | 'hostname'
+    | 'ipv4'
+    | 'ipv6'
+    | 'regex'
+    | 'uuid'
+    | 'json-pointer'
+    | 'relative-json-pointer';
 
-// NTB custom keywords
-readOnly?: boolean,
-noApiReturn?: boolean,
-availableForReferrers?: string[],
+  // NTB custom keywords
+  readOnly?: boolean,
+  noApiReturn?: boolean,
+  availableForReferrers?: string[],
 
-// define other keywords...
-
+  // define other keywords...
 }
 
 declare module 'objection' {
