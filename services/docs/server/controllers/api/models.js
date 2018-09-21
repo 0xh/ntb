aefc0228baa715eaf89fbed2ab512a6063bfdf95
Roar Skaar <roar.skaar@gmail.com>
2018-09-21 04:49:04 +0200

    fix(services/docs): adjust api to new model structure

--- services/docs/server/controllers/api/models.js
+++ services/docs/server/controllers/api/models.js
@@ -1,20 +1,20 @@
 import { Router } from 'express';
 
-import * as models from '../../ntb-old-models';
+import * as models from '@ntb/models';
 
 
 const router = new Router();
-const { BelongsToOneRelation } = models.BaseModel;
+const { BelongsToOneRelation } = models.Document;
 
 
 router.get('/', (req, res, next) => {
   const data = {};
 
   Object.values(models)
-    .filter((model) => model.APIEntryModel)
+    .filter((model) => model.apiEntryModel)
     .forEach((model) => {
       const relations = {};
-      Object.keys(model.relationMappings).forEach((relationKey) => {
+      Object.keys(model.relationMappings || {}).forEach((relationKey) => {
         const relation = model.relationMappings[relationKey];
         relations[relationKey] = {
           model: relation.modelClass,
@@ -41,7 +41,7 @@ router.get('/', (req, res, next) => {
       data[model.name] = {
         relations,
         idColumn: model.idColumn,
-        config: model.getAPIConfig(),
+        config: model.getApiConfigPerReferrer(),
         schema: model.jsonSchema,
         modelDescription: modelDescription || null,
       };
