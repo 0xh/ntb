b2ca4326e674314d081d853087101ba36bc14bdf
Roar Skaar <roar.skaar@gmail.com>
2018-09-28 03:55:02 +0200

    chore(services/api): lint expressAsyncHandler

--- services/api/lib/expressAsyncHandler.ts
+++ services/api/lib/expressAsyncHandler.ts
@@ -1,4 +1,9 @@
-import { RequestHandler, Request, Response, NextFunction } from 'express';
+import {
+  RequestHandler,
+  Request,
+  Response,
+  NextFunction,
+} from '@ntb/web-server-utils';
 
 
 export default function (fn: RequestHandler) {
